45ff4958b0fdb50cb1b41d8a71a55285e5bf522c
Sachin C <sachin1245@hotmail.com>
2016-05-13 00:50:34 +0530

    refactor api with heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -21,13 +23,8 @@ app.get('/todos',function(req,res){
 app.get('/todos/:id',function(req,res){
 
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodo;
-
-	todos.forEach(function(todo){
-		if(todoId === todo.id)
-			matchedTodo = todo;
-
-	})
+	
+	var matchedTodo = _.findWhere(todos,{id: todoId});
 
 	if(matchedTodo){
 		res.json(matchedTodo);
@@ -38,14 +35,22 @@ app.get('/todos/:id',function(req,res){
 })
 
 app.post('/todos',function(req,res){
-	var body = req.body;
+	var body = _.pick(req.body,'description','completed');
+	
+	if(!_.isBoolean(body.completed)  || !_.isString(body.description) || body.description.trim().length === 0)
+	{	
+		return res.status(400).send();
+	}
 
 	body.id = todoNextId++;
+
+	body.description = body.description.trim();
+	
 	todos.push(body);
 
 	console.log('description ' + body.description);
 
-	res.json(body);
+	res.json(body); 
 });
 
 app.listen(PORT,function(){
