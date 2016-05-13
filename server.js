cd5990321bb64667ecc0dd14325e083557443da7
Sachin C <sachin1245@hotmail.com>
2016-05-13 13:24:38 +0530

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -67,7 +67,37 @@ app.delete('/todos/:id',function(req,res){
 	todos = updatedTodos;
 
 	res.json(matchedTodo);
-})
+});
+
+app.put('/todos/:id',function(req,res){
+	var body = _.pick(req.body,'description','completed');
+	var todoId = parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos,{id: todoId});
+	var validAttributes = {};
+
+
+	if(!matchedTodo){
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	}else if(body.hasOwnPropery('completed')){
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	}else if(body.hasOwnPropery('description')){
+		return res.status(400).send();
+	}
+
+	
+	 _.extend(matchedTodo,validAttributes);
+
+	 res.json(matchedTodo);
+
+});
 
 app.listen(PORT,function(){
 	console.log('Express listening on PORT ' + PORT);
