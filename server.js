9109440fe1162ea9c492634ecbedd68ed6f5d0f1
Sachin C <sachin1245@hotmail.com>
2016-05-13 15:13:26 +0530

    Add ?completed filter

--- server.js
+++ server.js
@@ -14,13 +14,24 @@ app.use(bodyParser.json());
 
 
 app.get('/todos',function(req,res){
-	
-		res.json(todos);
+		
+		var queryParams = req.query;
+		var filteredTodos = todos;
+
+		if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+			filteredTodos = _.where(filteredTodos, {completed: true});
+		}else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+			filteredTodos = _.where(filteredTodos, {completed: false});
+		}
+
+
+		res.json(filteredTodos);
 	
 })
 
 //GET /todos/:id
 app.get('/todos/:id',function(req,res){
+
 
 	var todoId = parseInt(req.params.id,10);
 	
