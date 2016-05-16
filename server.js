27daaf40ff48c357a066b6641cfc74e6f58e3ce4
Sachin C <sachin1245@hotmail.com>
2016-05-16 17:07:11 +0530

    convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -17,26 +17,26 @@ app.use(bodyParser.json());
 app.get('/todos', function(req, res) {
 
 	var query = req.query;
-	var where ={};
+	var where = {};
 
-	if(query.hasOwnProperty('completed') && query.completed === 'true'){
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
-	}else if(query.hasOwnProperty('completed') && query.completed === 'false'){
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
 		where.completed = false;
 	}
 
-	if(query.hasOwnProperty('q') && query.q.length > 0){
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
 		where.description = {
-			$like: '%' + query.q +'%'
+			$like: '%' + query.q + '%'
 		};
-	}	
+	}
 
 	db.todo.findAll({
-		where:where
-	}).then(function(todos){
+		where: where
+	}).then(function(todos) {
 
 		res.json(todos);
-	},function(e){
+	}, function(e) {
 		res.status(500).send();
 	});
 
@@ -48,14 +48,14 @@ app.get('/todos/:id', function(req, res) {
 
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function(todo){
-		if(todo){
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
 			res.json(todo.toJSON());
-		}else{
+		} else {
 			res.status(404).json('not found');
 		}
-		
-	},function(e){
+
+	}, function(e) {
 		res.status(500).json(e);
 	});
 
@@ -64,10 +64,10 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	
-	db.todo.create(body).then(function(todo){
-		res.json(todo.toJSON());	
-	},function(e){
+
+	db.todo.create(body).then(function(todo) {
+		res.json(todo.toJSON());
+	}, function(e) {
 		res.status(400).json(e);
 	});
 });
@@ -75,19 +75,22 @@ app.post('/todos', function(req, res) {
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function(e) {
+		res.status(500).json(e);
 	});
 
-	if (!matchedTodo) {
-		return res.status(404).json('no todo item found for this id');
-	}
-
-	var updatedTodos = _.without(todos, matchedTodo);
-
-	todos = updatedTodos;
-
-	res.json(matchedTodo);
 });
 
 app.put('/todos/:id', function(req, res) {
